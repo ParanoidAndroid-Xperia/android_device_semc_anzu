80dda7421e71c78817de4aff171c56098ef09ddf
n1kolaa <n1kolaa@hotmail.com>
2013-03-07 14:19:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,9 +3,6 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/anzu/bluetooth
-
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_anzu_defconfig
@@ -18,7 +15,7 @@ SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
-TARGET_QCOM_HDMI_OUT := false
+TARGET_QCOM_HDMI_OUT := no
 
 BOARD_FLASH_BLOCK_SIZE := 2048
 
