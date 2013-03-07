b864b6334b38330ebd486c2aa50b47bcb8ecb23f
n1kolaa <n1kolaa@hotmail.com>
2013-03-07 14:16:22 +0100

    bluetooth fix part1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,7 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
+-include vendor/semc/anzu/BoardConfigVendor.mk
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/anzu/bluetooth
@@ -24,5 +25,6 @@ BOARD_FLASH_BLOCK_SIZE := 2048
 # Assert
 TARGET_OTA_ASSERT_DEVICE := LT18i,LT18a,LT15i,LT15a,anzu
 
-# inherit from the proprietary version
--include vendor/semc/anzu/BoardConfigVendor.mk
+#Bluetooth	
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/anzu/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/semc/anzu/bluetooth/vnd_anzu.txt
